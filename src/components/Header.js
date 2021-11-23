36984251272521109cf3aaba26ce53519d8ae43c
Jimoh Olalekan Ibrahim <horla_braheem@Jimohs-MacBook-Pro.local>
2021-11-23 17:47:52 +0100

    made new changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,55 @@
 import React, { useContext, useState } from "react";
 import { SidebarContext } from "../context/SidebarContext";
-import {
- 
-  BellIcon,
-  MenuIcon,
- 
-} from "../icons";
-import {
 
-  Dropdown,
-  DropdownItem,
-  // WindmillContext,
-} from "@windmill/react-ui";
-// import { useHistory } from "react-router-dom";
-// import { useAuth } from "../context/auth";
-// import { logout } from "../store/slices/auth";
-// import { useDispatch, useSelector } from "react-redux";
 
 function Header() {
-  //const { authTokens, setAuthTokens } = useAuth();
-  // const { mode, toggleMode } = useContext(WindmillContext);
+  
   const { toggleSidebar } = useContext(SidebarContext);
 
   const [isNotificationsMenuOpen, setIsNotificationsMenuOpen] = useState(false);
-  // const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  // const history = useHistory();
-
-  // const { admin } = useSelector((state) => state.auth);
-  // const dispatch = useDispatch();
+ 
 
   function handleNotificationsClick() {
     setIsNotificationsMenuOpen(!isNotificationsMenuOpen);
   }
 
-  // function handleProfileClick() {
-  //   setIsProfileMenuOpen(!isProfileMenuOpen);
-  // }
-  // const logOut = async () => {
-  //   dispatch(logout())
-  //     .unwrap()
-  //     .then(() => history.push("/login"))
-  //     .catch();
-  // };
+ 
 
   return (
     <header className="z-40 py-4 bg-primary  dark:bg-gray-800">
       <div className="container flex items-center justify-between h-full px-6 mx-auto text-white dark:text-pink-300">
         {/* <!-- Mobile hamburger --> */}
         <div className="flex items-center gap-2">
-        <button
-          className="p-1 mr-5 -ml-1 rounded-md lg:hidden focus:outline-none focus:shadow-outline-blue"
-          onClick={toggleSidebar}
-          aria-label="Menu"
-        >
-          <MenuIcon className="w-6 h-6" aria-hidden="true" />
-        </button>
-        <h1 className="font-thin">A2ZFIN</h1>
+          <button
+            className="p-1 mr-5 -ml-1 rounded-md lg:hidden focus:outline-none focus:shadow-outline-blue"
+            onClick={toggleSidebar}
+            aria-label="Menu"
+          >
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </span>
+          </button>
+          <h1 className="font-thin">A2ZFIN</h1>
         </div>
         <div className="flex justify-center flex-1 lg:mr-32"></div>
 
         <ul className="flex items-center justify-end flex-shrink-0 space-x-6">
           {/* <!--  toggler --> */}
-          <li className="flex">Olalekan</li>
-          
+          <li className="flex">John Doe</li>
+
           {/* <!-- Notifications menu --> */}
           <li className="relative">
             <button
@@ -71,7 +58,22 @@ function Header() {
               aria-label="Notifications"
               aria-haspopup="true"
             >
-              <BellIcon className="w-5 h-5" aria-hidden="true" />
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+              </span>
               {/* <!-- Notification badge --> */}
               <span
                 aria-hidden="true"
@@ -79,23 +81,8 @@ function Header() {
               ></span>
             </button>
 
-            <Dropdown
-              align="right"
-              isOpen={isNotificationsMenuOpen}
-              onClose={() => setIsNotificationsMenuOpen(false)}
-            >
-              <DropdownItem onClick={() => alert("Alerts!")}>
-                <span>Top up Notification</span>
-              </DropdownItem>
-              <DropdownItem onClick={() => alert("Alerts!")}>
-                <span>Traded BTC - USDT</span>
-              </DropdownItem>
-              <DropdownItem onClick={() => alert("Alerts!")}>
-                <span>Funded Naira wallet - ₦5.00</span>
-              </DropdownItem>
-            </Dropdown>
-          </li>
          
+          </li>
         </ul>
       </div>
     </header>
