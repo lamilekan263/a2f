f382cc41b3ab81c59749c2e7a0cba44d28bd7bb1
lamilekan263 <lamilekan263@gmail.com>
2021-11-28 10:16:17 +0100

    added the bridge page

--- src/pages/BridgePage.js
+++ src/pages/BridgePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Label,  Select, } from "@windmill/react-ui";
+import { Button, Input, Label, Select } from "@windmill/react-ui";
 
 const BridgePage = () => {
   return (
@@ -29,9 +29,9 @@ const BridgePage = () => {
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M17 8l4 4m0 0l-4 4m4-4H3"
                 />
               </svg>
@@ -44,6 +44,39 @@ const BridgePage = () => {
                 </Select>
               </Label>
             </div>
+            <div>
+            <Label>
+  <span>Token to Bridge</span>
+  <Input dir="rtl" className="mt-1 py-3 px-3" />
+</Label>
+            </div>
+            <div className="my-3">
+              <p>Bridgeable Range:</p>
+              <div className="flex justify-between my-2 text-xs">
+                <p>Max</p> <p>1,500,000 FONT</p>
+              </div>
+              <div className="flex justify-between my-2 text-xs">
+                <p>Min</p> <p>39.3 FONT</p>
+              </div>
+              <div className="flex justify-between my-2 text-xs">
+                <p>Fee</p> <p>0 FONT</p>
+              </div>
+              <p className="text-sm">
+                • Amounts greater than 300,000 FONT could take up to 12 hours
+              </p>
+
+              <p className="text-red-400 text-sm">
+                • Please connect your wallet to the chain you wish to bridge
+                from!{" "}
+              </p>
+            </div>
+
+            <Button
+              iconLeft={WalletIcon}
+              className="w-full mt-4 bg-primary mx-auto "
+            >
+              Connect Wallet
+            </Button>
           </form>
         </div>
       </div>
@@ -52,3 +85,22 @@ const BridgePage = () => {
 };
 
 export default BridgePage;
+
+const WalletIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+      />
+    </svg>
+  );
+};
